d571f4669f444c4fb57eb6c9535282f986a82f69
LApersona-cyber <hilarrandan011@student.unsrat.ac.id>
2026-03-07 00:50:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello world")
-print("AI Trading Lab started")
+gold_prices = [1980, 2010, 2030, 1975]
+
+for price in gold_prices:
+
+    print("Harga emas:", price)
+
+    if price > 2000:
+        print("Market: Bullish")
+    else:
+        print("Market: Bearish")
+
+    print("-----")
